7ad402d1dec405f66a9fb3aa7914a104b439e871
JackRuhala <ruhalaja@msu.edu>
2024-12-02 19:23:35 -0500

    Update PMMoV_Wastewater_Streamlit_Dashboard.py

--- PMMoV_Wastewater_Streamlit_Dashboard.py
+++ PMMoV_Wastewater_Streamlit_Dashboard.py
@@ -77,12 +77,12 @@ st.write('A big part of this dashboard is understanding how the environment chan
 st.title('Available Data')
 st.markdown('''If you have not already please read the introduction on wastewater epidemiology and PMMoV.
             If you dont have time to read the into or dont understand the context of what it means, here is a gross oversimplification of some of the more complicated data variables
-            * PMMoV, Pi6, N1, N2, are all viruses
-            * gc/100ml = number of viruses per 100ml of collected sample
-            * gc/100ml = f(ct^-1)
-            * PMMoV = Human fecal contamination
-            * Pi6 = Extraction control virus for N1 and N2
-            * N1 = N2 = COVID-19 (no specific variant) '''
+            - PMMoV, Pi6, N1, N2, are all viruses
+            - gc/100ml = number of viruses per 100ml of collected sample
+            - gc/100ml = f(ct^-1)
+            - PMMoV = Human fecal contamination
+            - Pi6 = Extraction control virus for N1 and N2
+            - N1 = N2 = COVID-19 (no specific variant) '''
 )
 
 # BEER_df = BEER_df.rename(columns={'Name': 'Name of Beer', 'Style_x':'Brewing Style', 'Style Color Mean': 'Style Color Mean (SRM)', 'Style Color Var':'Style Color Var (SRM)'})
