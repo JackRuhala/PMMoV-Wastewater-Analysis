81b712b66d10146a4ae6d8401b38a0347db9871d
JackRuhala <ruhalaja@msu.edu>
2024-12-02 23:19:47 -0500

    Update PMMoV_Wastewater_Streamlit_Dashboard.py

--- PMMoV_Wastewater_Streamlit_Dashboard.py
+++ PMMoV_Wastewater_Streamlit_Dashboard.py
@@ -157,7 +157,6 @@ if user_input_1 in WW_df['Code'].values:
          st.write(user_input_1, ' : Code Not Found.')
 
     fig = px.scatter(Code_data, x='Discharge (ft^3/s)', y='FlowRate (MGD)', title=f"Discharge vs FlowRate for {user_input_1}")
-    fig.add_trace(go.Scatter(x=Code_dis, y=(Code_dis * w1 + w0)))
     fig.show()
 
 else:
