03b57d0734c2862655a219f305941328143e4595
JackRuhala <ruhalaja@msu.edu>
2024-12-07 15:17:55 -0500

    Update Home.py

--- apps/Home.py
+++ apps/Home.py
@@ -14,10 +14,18 @@ def app():
     One set of information will be targeted toward those with no background in molecular biology and the other set will contain more detailed information. 
     The page that explains the data for those with no molecular biology background will be oversimplified and will present some concepts incorrectly so that the bigger picture is better understood.
     ''')
-    
 
-    st.markdown("### Sample Data")
-    df = create_table()
-    st.write(df)
+    st.title('Special Data Source Acknowledgement')
+    st.write('''
+    This repository focuses on the analysis of viruses extracted from wastewater in Kent County Michigan. 
+    While some of the data used for this app was collected from various government agencies (links to the data below), all viral wastewater tracking data was provided by the Grand Valley State University Molecular monitoring team. 
+    This repository was given permission by the Grand Valley State lab to make all the data used in this app public. 
+    This app will only explore a small aspect of the work being done on wastewater epidemiology so there may be some questions you might have about the data that will only be explained once the Grand Vally team decides to publish their resalts. 
+    The conclusions of data made in this app does not speak for the conclusions of the Grand Vally team, although some discussion has taken place to align our thoughts and ideas on the data. 
+    I would like to thank the Grand Valley State University Molecular monitoring team for permitting me to use their data and I would encourage others interested in this research to read the publication when it becomes available.
+    ''')
 
-    st.write('Navigate to `Data Stats` page to visualize the data')
+    st.write('''
+    On the left side menu, go to the ether the basic introduction or the extensive introducton to learn more about the data.
+    Click on the data tabs to see spicifc analysis of the data.
+    ''')
