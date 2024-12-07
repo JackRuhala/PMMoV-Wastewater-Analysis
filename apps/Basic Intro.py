6b23c4f8a0af519c492b2cf364d309e8bfeab7e1
JackRuhala <ruhalaja@msu.edu>
2024-12-07 15:37:18 -0500

    Update Basic Intro.py

--- apps/Basic Intro.py
+++ apps/Basic Intro.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+def app():
+    st.title('Basic intro')
+
+    st.write(''' 
+          Humans are a complex ecosystem of viruses, bacteria, and cells all sharing one body. 
+          Sometimes viruses and bacteria don’t play nicely with our bodies and make us sick.
+          While our bodies have some cool defense systems against disease, our infections can still slip past our defense and often can be found in unexpected places.
+          One such place our illnesses can wine up is in our poop.
+          poop, being covered in germs is not a new idea, but testing poop for specific infections is a resent development.
+          However if an individual want to know if they have a disease or not, there are normally easier ways to test that then sending in a poop sample to a lab.
+          While poop testing might be inefficient for an individual, what about testing a population?
+          Americans are lucky enough to have a sewer system that carries away the waste we don’t like to think about, but there’s valuable information about disease in that sewer water.
+          Wastewater epidemiology targets the collective fecal matter in the sewer system and tests the water for specific diseases.
+          Wastewater epidemiology provides a way to gain valuable knowledge about a disease in an environment without needing to test all individuals in a population.
+          ''')
