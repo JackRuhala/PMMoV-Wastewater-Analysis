bd3dd71b4527e8ecf6b8acb24185a9cb9661da34
JackRuhala <ruhalaja@msu.edu>
2024-12-07 16:04:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from apps import home, data_stats # import your app modules here
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Home", Home.app)
-app.add_app("Basic Intro", Basic Intro.app)
-app.add_app("Extensive Intro", Extensive intro.app)
+app.add_app("Home", home.app)
+app.add_app("Basic Intro", basic_intro.app)
+app.add_app("Extensive Intro", extensive_intro.app)
 
 # The main app
 app.run()
