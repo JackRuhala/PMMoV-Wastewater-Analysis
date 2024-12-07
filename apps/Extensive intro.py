91adf30406182006544c60c002b4e5d5cb981abd
JackRuhala <ruhalaja@msu.edu>
2024-12-07 15:37:52 -0500

    Create Extensive intro.py

---
+++ apps/Extensive intro.py
@@ -0,0 +1,18 @@
+Wastewater epidemiology is a new industry that aims to track vectors of disease through non-invasive means.
+         There are multiple methods of detecting disease through wastewater but the data we have uses a centrifugal extraction method with florescent PCR detection.
+         The workflow of wastewater epidemiology is as follows.
+         The prosess starts by collecting a sample of wastewater from a sewer.
+         A sample can be collected is one of two ways, by a composite extractor or a direct grab collection.
+         A composite extractor is a collection tank that collects a small portion every few min, over the course of a day, while a grab is just direct water collection out of a manhole.
+         Once the sample is collected it goes to a lab for processing.
+         The lab takes a portion of the sample collected and concentrates the sample by separating the water from the vector of disease.
+         Contention involves trapping viruses in a net made of polyethylene glycol (PEG), removing as much water from the PEG as possible, then extracting the collected viruses from the PEG.
+         Once the viruses are extracted, a small portion of the extraction is used to count the number of viruses in a sample.
+         Viral detection counts are recorded in two different forms depending on the detection method used.
+         The qPCR method records viral count as cycles (CT).
+         CT data is compared to a viral count standard which converts CT to gene copy data (gc) and gc is assumed to be an approximant equivalent of virus in a sample.
+         The other detection method id ddPCR which produces direct gc data.
+         There is more nuance and scientific reasoning behind the actual collection of data but knowing more than the basics should not be required for understanding the data.
+         In short, from start to finish, sample collection and viral detection is very involved and volatile so making notes of all variables between collection to detection is important for accurate data presentation.
+         The goal of this dashboard is to determine what factors impact viral detection in order to generate more accurate predictions of disease in a local population
+          ''')
