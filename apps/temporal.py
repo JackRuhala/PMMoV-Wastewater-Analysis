e0eae8a0215b81c9b6e3c906cadbc7fc6354fc83
JackRuhala <ruhalaja@msu.edu>
2024-12-08 18:14:09 -0500

    Update temporal.py

--- apps/temporal.py
+++ apps/temporal.py
@@ -96,7 +96,7 @@ def app():
          # Cross correlation code was written by ChatGPT4.0 mini but manually changed and checked over time to fit the streamlit app
          accuracy_test_df = WW_df[WW_df['Code'] == 'GR']
          # user inputs number of lag steps for plot
-         user_input_GR_lag = st.text_input('Enter a number of lags for site GR', '8')
+         user_input_GR_lag = slider_value = st.slider("Select a value", min_value=-20, max_value=20, step=1, value=8)
          user_input_GR_lag = int(user_input_GR_lag)
          accuracy_test_df = accuracy_test_df.loc[(accuracy_test_df['Date'] >= '2023-09-01') & (accuracy_test_df['Date'] <= '2024-04-01')]
          # remove columns with missing data
