0731c117a92b3ce94d76369f16098d2c27cf5f93
JackRuhala <ruhalaja@msu.edu>
2024-12-07 19:38:16 -0500

    Update extensive_intro.py

--- apps/extensive_intro.py
+++ apps/extensive_intro.py
@@ -24,3 +24,22 @@ def app():
          Both CT and GC data are back calculated to estimate the amount of viral gene copys in the origanl 100ml sample fraction.
          The problem this dashboard is trying to explore is how to validate the viral gene count data to sample fecal contamination and public instancis of spicifc disease.
           ''')
+    st.title('What is PMMoV?')
+    st.write('''
+        A big part wastewater epidemiology is controling how the environment changes the detection of viruses in wastewater.
+        Studying the environments effect on the disease of interest in nearly imposable due to the true instance of a particuler disease in a population is never truly known.
+        Instead of studying the effect of the environment on the disease directly, we can indirectly infer how the environment affects disease detection through a fecal matter variable.
+        Currently there are a number of proposed and established fecal matter markers, (HBFs), in literature from detection of human genes, bacterial genes, surface antigens, viruses, and caffein concentration.
+        The fecal matter control in our data is a plant virus called pepper mild monotilo virus or PMMoV for short.
+        PMMoV spreads from peppers, or processed pepper spices to human through consumption.
+        PMMoV endures the human digestive tract and is harmlessly expelled through our fecal matter where it enters the water and eventually infects more pepper plants.
+        Because PMMoV is expelled through human waste, PMMoV concentration has been found to be strongly positively correlated to human waste, and because pepper consumption is common in America most human waste contains PMMoV.
+        PMMoV also has the added benefit of being a virus, so PMMoV should exibits similer dynamic propertys as the other virusis we want to track.
+        Although plant viruses have unique morphology compared to human viruses PMMoV is suspected of behaving similarly to viruses of interest during the collection, extraction and detection processes.
+        For all of the reasons listed above, PMMoV detection is interpreted as the same as human fecal contamination data.
+        The higher the PMMoV counts, the more fecal matter in a sample, the higher the suspected count of disease.
+        If PMMoV counts change with environmental factors, then the suspected count of disease will positively corelate with the change in PMMoV.
+        The goal of this data is to show if PMMoV contamination is  positive correlation to human diseases that can be track through waste water.
+''')
+
+    st.image('TMV.png', caption="This is an image of the Tobaco Mosaic virus, a close ansester of PMMoV, PMMoV and TMV are both rod shaped virused. Image was taken from https://www.semanticscholar.org/paper/The-physics-of-tobacco-mosaic-virus-and-virus-based-Alonso-G%C3%B3rzny/3177b81019a98aa9c2a17be46f325d1033f96f13")
