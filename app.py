5ab9a4ea305af6b42eacc79d7f1248f3713e89af
JackRuhala <ruhalaja@msu.edu>
2024-12-08 17:47:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, basic_intro, extensive_intro, data_cleaning_and_presentaion, environment, regression # import your app modules here
+from apps import home, basic_intro, extensive_intro, data_cleaning_and_presentaion, environment, regression, lab_preformace # import your app modules here
 
 app = MultiApp()
 
@@ -11,6 +11,7 @@ app.add_app("Extensive Intro", extensive_intro.app)
 app.add_app("Avalible Data", data_cleaning_and_presentaion.app)
 app.add_app("Enviromental Data", environment.app)
 app.add_app("Enviromental PMMoV correlation and Regression models", regression.app)
+app.add_app("lab Veriables", lab_preformace.app)
 
 # The main app
 app.run()
