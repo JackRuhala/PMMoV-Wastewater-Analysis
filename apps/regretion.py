b8ce213c7b014360233089664f9a1981e4793bc8
JackRuhala <ruhalaja@msu.edu>
2024-12-08 13:18:12 -0500

    Create regretion.py

---
+++ apps/regretion.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+# import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+import plotly.graph_objects as go
+from scipy import stats
+from sklearn.preprocessing import LabelEncoder
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.linear_model import LinearRegression
