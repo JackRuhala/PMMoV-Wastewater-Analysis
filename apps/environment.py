7af09662b729e56005395622a512eb1805e08013
JackRuhala <ruhalaja@msu.edu>
2024-12-08 12:57:53 -0500

    Update environment.py

--- apps/environment.py
+++ apps/environment.py
@@ -24,21 +24,31 @@ def app():
                   Below, take some time to look at how the environment of a sewer system changes with the environment outside.
                   '''
          )
-         
-         fig2 = px.scatter(WW_df, title = 'Kent County Sewer Water Tempature', x='Date', y='Temp', render_mode='svg')
+
+         # loacl water temp graph
+         fig1 = px.scatter(WW_df, title = 'Kent County Sewer Water Tempature', x='Date', y='Temp', render_mode='svg')
+         st.plotly_chart(fig1)
+
+         # local snow depth graph
+         fig2 = px.scatter(WW_df, title = 'local Snow depth in inches', x='Date', y='SNDW (Snow Depth)', render_mode='svg')
          st.plotly_chart(fig2)
          
-         # univariate graphs of grand river discharge vs flow rate vs precipitation and snow melt
-         fig3 = px.scatter(WW_df, title = 'Sewer Flow Rate by site', x='Date', y='FlowRate (MGD)', color ='Code', render_mode='svg')
+         # local Precipitaion graph
+         fig3 = px.scatter(WW_df, title = 'local Precipitaion in inches', x='Date', y='PRCP (Rain fall in)', render_mode='svg')
+         st.plotly_chart(fig2)
+         
+         # Grand River graph
+         fig4 = px.scatter(WW_df, title = 'Discharge of the Grand River (ft^3/s)', x='Date', y='Discharge (ft^3/s)')
          st.plotly_chart(fig3)
          
-         # fig4 = px.scatter(WW_df_standerd, title = 'Sewer Flow Rate vs Enviroment', x='Date', y='FlowRate (MGD)')
-         # fig4.add_scatter(WW_df_standerd, x='Date',y='Discharge (ft^3/s)')
-         # st.plotly_chart(fig4)
-         # univariate graphs of pH of a system
-         fig5 = px.scatter(WW_df, title = 'Sewer Water pH by site', x='Date', y='pH', color ='Code', render_mode='svg')
-         st.plotly_chart(fig5)
-         # univariate graphs of PMMoV recorded in a system
+         # Sewer flow rate graph
+         fig5 = px.scatter(WW_df, title = 'Sewer Flow Rate by site', x='Date', y='FlowRate (MGD)', color ='Code', render_mode='svg')
+         st.plotly_chart(fig4)
          
-         fig5 = px.scatter(WW_df, title = 'PMMoV Gene Copys recorded in 100ml Sewer Water sample', x='Date', y='PMMoV (gc/ 100mL)', color ='Code', render_mode='svg')
+         # pH of a system graph
+         fig6 = px.scatter(WW_df, title = 'Sewer Water pH by site', x='Date', y='pH', color ='Code', render_mode='svg')
          st.plotly_chart(fig5)
+         
+         # graphs of PMMoV recorded in a system
+         fig7 = px.scatter(WW_df, title = 'PMMoV Gene Copys recorded in 100ml Sewer Water sample', x='Date', y='PMMoV (gc/ 100mL)', color ='Code', render_mode='svg')
+         st.plotly_chart(fig6)
